221026611b316c8d545b4d92c716d3c8595e5ef8
Sanele <sanelevuza@hexaura.com>
2025-06-04 11:33:36 +0200

    first commit

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
-from twilio.twiml.messaging_response import MessagingResponse #imports message response 
+from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request 
 import os
-import requests, random
+import requests
+import random
 from dotenv import load_dotenv
 
+#usrDict = {}
+#load_dotenv()
+app = Flask(__name__)
 
-load_dotenv()
-
-app = Flask(__name__) #innitialize the name of the app
-
-@app.route("/", methods=["Post", "Get"])
+@app.route("/", methods=["POST"])
 def chatbot():
-    
-    inc_msg = request.form.get("Body", "").lower()
+        #user input 
+    user_msg = request.values.get("Body", "").lower
 
-    resp = MessagingResponse()
-    message= resp.message()
-    #print(dir(message))
+        #creating an object of MessagingResponse
+    response = MessagingResponse()
 
-    if "cat" in inc_msg: 
-        message.media("https://cataas.com/cat ")
-    elif "quote" in inc_msg:
-        api_url = 'https://api.api-ninjas.com/v1/quotes'
-        response = requests.get(api_url, headers={
-    'X-Api-Key': os.environ.get('API-KEY')
-    })
-        if response.status_code == requests.codes.ok:
-            #print(response.text)
+     #user query 
+    q = user_msg
 
-            datalist = response.json()
-            data = random.choice(datalist)
-            # print(data)
-            message.body(f'"Quote: {data["quote"]} - {data["author"]}')
-        else:
-            print("Error:", response.status_code, response.text)
-    else: 
-        message.body("I can only provide cat images and famous quotes. Type cat or quote.")
+     #list to store 
+    result = [1,2,3]
+    i = 0
+     # searching and storing 
+    for i in result:
+        
+        result.append(i+1)
 
-    return str(resp)
+    #display results 
+    msg = response.message("Welcome to the Pothole Reporter! Please describe where the pothole is located (e.g., street name, nearest intersection, landmark).")
 
+    if result[i] == 0:
+        msg = response.message("Welcome to the Pothole Reporter! Please describe where the pothole is located (e.g., street name, nearest intersection, landmark).")
+
+    elif result[i] == 1:
+        msg = response.message("Thank you! Now, please share your location by using WhatsApp's location sharing feature.")  
+
+    elif result[i] == 2:
+        msg = response.message("Got your location! Finally, please take and send a photo of the pothole.")
+     
+    else:
+        msg = response.message("Thank you for reporting the pothole! Your report has been received and saved.")
+
+    return str(response)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
